135e5df0488677ee6ba092ea57fce25e66b0683e
davidtc8 <davidtorresc8@gmail.com>
2021-01-19 18:12:26 -0600

    Enemy class for the game

--- enemy_class.py
+++ enemy_class.py
@@ -1,3 +1,5 @@
+import random
+
 # We're constructing a enemy class
 class Enemy:
     def __init__(self, Ehealth, Eattack, Especial, Echance, Ename):
@@ -58,3 +60,35 @@ class Boss(Enemy):
         self.superMove = newSuperMove
 
 # If I want to create a last boss or something I can just create an inheritance of Boss with the super method
+
+# creating a function to create enemies
+def enemygen(levelBoss):
+    temp = []
+    with open('adjective.txt', 'r') as file:
+        lines = file.readlines()
+        # as we want to choose a random adjective, our adjective is going to be between
+        # the first adjective (0) and the lenght of the file -1 because of how Python operates the numbers
+        # we're putting the [:-1] to eliminate from the text the '/n' that appears in each letter
+        adjective = lines[random.randint(0, len(lines)-1)][:-1]
+    with open('animal.txt', 'r') as file2:
+        lines2 = file2.readlines()
+        animal = lines2[random.randint(0, len(lines)-1)][:-1]
+
+    if levelBoss == False:
+        health = random.randint(50, 100)
+        attack = random.randint(1,10)
+        special = random.randint(10,20)
+        chance = random.randint(1,10)
+
+        # In here we're saying: call the class Enemy and give those attributes
+        return Enemy(health, attack, special, chance, adjective + " " + animal)
+
+    else:
+        health = random.randint(200, 250)
+        attack = random.randint(20, 40)
+        special = random.randint(50, 60)
+        chance = random.randint(1, 8)
+        superMove = random.randint(100,200)
+
+        # Return the Boss object and give it those attributes
+        return Boss(health, attack, special, chance, adjective + " " + animal, superMove)
